0226b427bd0f94c5b78730ba8cb5870bc7ab5e65
Zhou <yingze.zhou.ext@nokia-sbell.com>
2018-11-13 17:24:00 +0800

    提取tag组件

--- src/pages/components/Article.js
+++ src/pages/components/Article.js
@@ -1,36 +1,19 @@
 import { Component } from 'react';
-import { connect } from 'dva';
 import Link from 'umi/link'
 import {Icon} from 'antd'
 import styles from './Article.css'
 import tools from '../../common/Tools'
-import router from 'umi/router';
-
-new Date().toLocaleDateString();
+import Tag from './Tag'
 class Article extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
-        this.searchTag = this.searchTag.bind(this);
-    }
-    searchTag (id){
-        const { dispatch } = this.props;
-		dispatch({
-            type: 'global/getSearch',
-            payload:{
-                id
-            }
-        });
-        router.push('/class');
     }
     render() {
         const article = this.props.article;
         const describe = article.article_describe 
                         ? article.article_describe
                         : article.article_content.slice(0,50) + '...' ;
-        const tags = article.article_tags && article.article_tags.length > 0
-                     ? article.article_tags.map((tag, index) => <a key={index} onClick={() => this.searchTag(tag.tagid)} className={styles.icons_tag}>{tag.tag.name}</a>)
-                     : '暂无标签'
         return (  
             <div className={styles.container}>
                 <div className={styles.title} title={article.article_title}><Link to={`article?id=${article.id}`}>{article.article_title}</Link></div>
@@ -43,14 +26,11 @@ class Article extends Component {
                     <span title="观看次数"><Link to={`article?id=${article.id}`}><Icon type="fire" theme="filled" />{article.article_click}</Link></span>
                     <span title="评论数"><Link to={`article?id=${article.id}#message`}><Icon type="smile" theme="filled" />{article.comment}</Link></span>
                     <span style={{cursor:'default',verticalAlign:'1px'}}>/</span>
-                    <span style={{cursor:'default'}}>标签: {tags}</span>
+                    <Tag />
                 </div>
             </div>
         );
     }
 }
-function mapStateToProps(state) {
-	return {
-	};
-}
-export default connect(mapStateToProps)(Article);
+
+export default Article;
---
+++ src/pages/components/Tag.js
@@ -0,0 +1,35 @@
+import { Component } from 'react';
+import { connect } from 'dva';
+import router from 'umi/router';
+class Tag extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  
+        }
+        this.searchTag = this.searchTag.bind(this);
+    }
+    searchTag (id){
+        const { dispatch } = this.props;
+		dispatch({
+            type: 'global/getSearch',
+            payload:{
+                id
+            }
+        });
+        router.push('/class');
+    }
+    render() { 
+        const article_tags = this.props.article_tags
+        const tags = article_tags && article_tags.length > 0
+                    ? article_tags.map((tag, index) => <a key={index} onClick={() => this.searchTag(tag.tagid)} className={styles.icons_tag}>{tag.tag.name}</a>)
+                    : '暂无标签'
+        return (  
+            <span style={{cursor:'default'}}>标签: {tags}</span>
+        );
+    }
+}
+function mapStateToProps(state) {
+	return {
+	};
+}
+export default connect(mapStateToProps)(Tag);
